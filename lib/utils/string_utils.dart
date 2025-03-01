d511d908d2dfc00e2c835d41fc812c5042e21e8a
Yung Hak Lee <celebi3@naver.com>
2025-03-02 01:49:24 +0900

    Feat: 성능 향상을 위한 코드 변경, 불필요한 파일 삭제, 포켓몬 도감 설명 추가
    - pokedex_entry model: pokedex api로 id, name 추가
    - pokemon_detail: PokemonDetail클래스 추가
    - Pokemon_List,Result 삭제
    - pokemon_detail_screenPokemonDetailScreen 도감 내용 추가(설명, 키/몸무게, 타입)
    - pokemon_list_screen: PokemonListScreen pokedex api로 새로운 id, name를 가져오는 코드로 변경.
    -ApiService에 pokedexEntry, pokemonDetail 추가 및 일부 코드 변경
    -string_utils: capitalization 메서드 변경(리스트 형태 문자열 쉼표 추가 될 때마다 대문자 변경)

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -1,9 +1,21 @@
-import 'package:pokedex_app/models/pokemon_list_result.dart';
+import 'package:pokedex_app/models/pokedex_entry.dart';
 
 extension StringCapitalize on String {
+  // 소문자로 시작하면 대문자로 전부 대문자면 앞 글자만 빼고 소문자로 바꿔줌
   String capitalize() {
     if (isEmpty) return this;
     return '${this[0].toUpperCase()}${substring(1).toLowerCase()}';
+  }
+
+  // 쉼표로 구분된 문자열 처리
+  String capitalizeList() {
+    if (isEmpty) return this;
+    // 쉼표와 공백(`, `)으로 나눔
+    final words = split(', ');
+    // 각 단어를 capitalize()로 변환
+    final capitalizedWords = words.map((word) => word.capitalize()).toList();
+    // 다시 쉼표와 공백으로 합침
+    return capitalizedWords.join(', ');
   }
 }
 
@@ -16,8 +28,8 @@ class ShinyPokemon {
   }
 
   //포켓몬id로 이미지 url을 반환
-  String getPokemonImageUrl(PokemonListResult pokemon) {
-    final pokemonId = pokemon.getPokemonId();
+  String getPokemonImageUrl(PokedexEntry id) {
+    final pokemonId = id.id;
     if (isShiny) {
       return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/$pokemonId.png';
     } else {
