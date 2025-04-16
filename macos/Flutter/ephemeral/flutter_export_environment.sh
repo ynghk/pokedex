913001c8d53284c7f90e3b04377726fb376eb5b7
YNGHK <celebi3@naver.com>
2025-04-17 08:57:07 +0900

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Desktop\app_develop\poke_master"
+export "FLUTTER_ROOT=/Users/ynghk/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ynghk/Documents/appschool7th/flutter_practice/poke_master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
